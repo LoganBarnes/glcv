2d2e6d17d51b90c09174f892fc63b276bd7ddf07
Logan Barnes <barneslt63@gmail.com>
2018-06-24 23:21:37 +0100

    Update ci links

--- .ci/download_vulkan_linux.sh
+++ .ci/download_vulkan_linux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 pushd $1
 VULKAN_VERSION=1.1.77.0
-VULKAN_TAR_FILE="vulkansdk-linux-x86_64-$VULKAN_VERSION.tar.gz"
-VULKAN_URL="https://vulkan.lunarg.com/sdk/download/$VULKAN_VERSION/linux/$VULKAN_TAR_FILE"
+VULKAN_TAR_FILE="vulkansdk-linux-x86_64-${VULKAN_VERSION}.tar.gz"
+VULKAN_URL="https://sdk.lunarg.com/sdk/download/${VULKAN_VERSION}/linux/${VULKAN_TAR_FILE}?Human=true;u="
 mkdir vulkan-linux \
 && travis_retry wget --no-check-certificate -O - ${VULKAN_URL} \
 | tar --strip-components=1 -xz -C vulkan-linux
--- .ci/download_vulkan_mac.sh
+++ .ci/download_vulkan_mac.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 pushd $1
-VULKAN_VERSION=1.1.73.0
-VULKAN_TAR_FILE="vulkansdk-macos-$VULKAN_VERSION.tar.gz"
-VULKAN_URL="https://vulkan.lunarg.com/sdk/download/$VULKAN_VERSION/mac/$VULKAN_TAR_FILE"
+VULKAN_VERSION=1.1.77.0
+VULKAN_TAR_FILE="vulkansdk-macos-${VULKAN_VERSION}.tar.gz"
+VULKAN_URL="https://sdk.lunarg.com/sdk/download/${VULKAN_VERSION}/mac/${VULKAN_TAR_FILE}?Human=true;u="
 mkdir vulkan-mac \
 && travis_retry wget --no-check-certificate -O - ${VULKAN_URL} \
 | tar --strip-components=1 -xz -C vulkan-mac
