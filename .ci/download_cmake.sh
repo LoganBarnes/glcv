11d5199f8a111138db33f55da578df3bd9d204a6
Logan Barnes <barneslt63@gmail.com>
2018-06-24 04:12:47 +0100

    Move cmake download to script

---
+++ .ci/download_cmake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+pushd $1
+CMAKE_VERSION=3.11
+CMAKE_TAR_FILE="cmake-CMAKE_VERSION.0-Linux-x86_64.tar.gz"
+CMAKE_URL="https://cmake.org/files/vCMAKE_VERSION/$CMAKE_TAR_FILE"
+mkdir cmake \
+&& travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} \
+| tar --strip-components=1 -xz -C cmake
+export PATH=$1/cmake/bin:${PATH}
+popd
